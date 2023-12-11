3df79556313ca32bb7e313f15a92b02443b4938b
Luma Gabino <lumagabino@gmail.com>
2023-12-11 02:41:59 -0300

    More images to carousel & Resposive footer

--- Home/events.js
+++ Home/events.js
@@ -3,32 +3,40 @@ let currentItem = 0;
 const items = document.querySelectorAll(".item-gallery");
 const maxItems = items.length;
 
-controls.forEach((control) => {
-  control.addEventListener("click", (e) => {
-    isLeft = e.target.classList.contains("arrow-left");
+function clickCarouselLeft() {
+    clickCarouselControl(true);
+}
+
+function clickCarouselRight() {
+    clickCarouselControl(false);
+}
+
+function clickCarouselControl(isLeft) {
+    console.log("isLeft:", isLeft);
 
     if (isLeft) {
-      currentItem -= 1;
+    currentItem -= 1;
     } else {
-      currentItem += 1;
+    currentItem += 1;
     }
 
     if (currentItem >= maxItems) {
-      currentItem = 0;
+    currentItem = 0;
     }
 
     if (currentItem < 0) {
-      currentItem = maxItems - 1;
+    currentItem = maxItems - 1;
     }
 
     items.forEach((item) => item.classList.remove("current-item"));
 
     items[currentItem].scrollIntoView({
-      behavior: "smooth",
-      block: 'nearest',
-      inline: "center"
+    behavior: "smooth",
+    block: 'nearest',
+    inline: "center"
     });
 
     items[currentItem].classList.add("current-item");
-  });
-});
+
+}
+
